b1c95dec130ada0806e7fc10338e63039a6e2fb9
Digital Studio ID <digitalstudio.id@gmail.com>
2018-06-20 13:11:23 +0700

    Clear Syntax
    Clear Syntax

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+/*
+
+Reoptimized class loader: php artisan optimize
+Clear Cache facade value: php artisan cache:clear
+Clear Route cache: php artisan route:cache
+Clear View cache: php artisan view:clear
+Clear Config cache: php artisan config:cache
+
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
